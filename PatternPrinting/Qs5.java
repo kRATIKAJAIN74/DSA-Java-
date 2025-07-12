343ba447464e70f063c25145679201e4f1c3e1df
Kratika Jain <jkratika8@gmail.com>
2025-07-12 12:23:11 +0530

    done

---
+++ PatternPrinting/Qs5.java
@@ -0,0 +1,17 @@
+package PatternPrinting;
+import java.util.Scanner;
+public class Qs5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print(j);
+            }
+            for(int k=1;k<=i-1;k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
