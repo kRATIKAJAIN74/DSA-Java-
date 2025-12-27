51fe681e14314ff2cfc07bdec5b701a12a70d776
Kratika Jain <jkratika8@gmail.com>
2025-12-27 19:34:42 +0530

    done

---
+++ GreedyAlgorithm/ActivitySelection.java
@@ -0,0 +1,18 @@
+package GreedyAlgorithm;
+import java.util.*;
+public class ActivitySelection {
+  public static void main(String[] args) {
+    int start[] = {1,3,0,5,8,5};
+    int end[] = {2,4,6,7,9,9};
+
+    int maxAct = 0;
+    ArrayList<Integer> ans = new ArrayList<>();
+    //1st activity
+//     maxAct = 1;
+//     ans.add(0);
+// int lastEnd = end[0];
+//     for()
+
+  }
+}
+
