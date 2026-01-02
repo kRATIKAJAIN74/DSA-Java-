2374ec3178b10dc1066a19fdeb6de80324eb2fa2
Kratika Jain <jkratika8@gmail.com>
2026-01-02 15:05:15 +0530

    done

---
+++ Heap/Insert.java
@@ -0,0 +1,78 @@
+package Heap;
+import java.util.*;
+public class Insert {
+  static class heap{
+    ArrayList<Integer> arr = new ArrayList<>();
+    
+    
+    public void add(int data){
+      //add at last idx
+      arr.add(data);
+      int x = arr.size()-1;
+      int par = (x-1)/2;
+      while( x>0 && arr.get(x)<arr.get(par)){  //O(logn)
+        int temp = arr.get(x);
+        arr.set(x, arr.get(par));
+        arr.set(par, temp);
+
+        x = par;
+        par = (x-1)/2;
+      }
+    }
+     public int peek(){
+      return arr.get(0);
+     }
+
+     private void heapify(int i){
+      int left = 2*i+1;
+      int right = 2*i+2;
+      int minIdx = i;
+
+      if(left<arr.size() && arr.get(minIdx)>arr.get(left)){
+        minIdx = left;
+      }
+      if(right<arr.size() && arr.get(minIdx)>arr.get(right)){
+        minIdx = right;
+      }
+      if(minIdx!=i){
+        //swap
+        int temp = arr.get(i);
+        arr.set(i, arr.get(minIdx));
+        arr.set(minIdx, temp);
+
+        heapify(minIdx);
+      }
+     }
+
+     public int remove(){
+      int data = arr.get(0);
+      //step 1 swap 1st and last
+      int temp = arr.get(0);
+      arr.set(0, arr.get(arr.size()-1));
+      arr.set(arr.size()-1, temp);
+
+      //step 2 remove last node
+      arr.remove(arr.size()-1);
+
+      //heapify
+   heapify(0);
+   return data;
+     }
+      
+     public boolean isEmpty(){
+      return arr.size()==0;
+     }
+  }
+  public static void main(String[] args) {
+    heap h = new heap();
+    h.add(3);
+    h.add(4);
+    h.add(1);
+    h.add(5);
+
+    while(!h.isEmpty()){
+      System.out.println(h.peek());
+      h.remove();
+    }
+  }
+}
