175258119474f5ca7433f602466147bf39d74521
Kratika Jain <jkratika8@gmail.com>
2026-01-20 22:54:14 +0530

    done

--- Graph/dijkstra.java
+++ Graph/dijkstra.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class dijkstra {
   static class Edge{
@@ -32,10 +33,58 @@ public class dijkstra {
     
  
   }
+  static class Pair implements Comparable<Pair>{
+    int n;
+    int path;
+
+    public Pair(int n, int path){
+      this.n = n;
+      this.path = path;
+    }
+    @Override
+    public int compareTo(Pair p2){
+      return this.path-p2.path;
+    }
+  }
+  public static void dijkstra(ArrayList<Edge> graph[], int src){
+  int dist[] = new int[graph.length];
+  for(int i=0;i<graph.length;i++){
+    if(i!=src){
+  dist[i] = Integer.MAX_VALUE;
+    }
+  }
+  boolean vis[] = new boolean[graph.length];
+  PriorityQueue<Pair> pq = new PriorityQueue<>();
+  pq.add(new Pair(src, 0));
+  while(!pq.isEmpty()){
+    Pair curr = pq.remove();
+    if(!vis[curr.n]){
+      vis[curr.n] = true;
+      for(int i=0;i<graph[curr.n].size();i++){
+      Edge e = graph[curr.n].get(i);
+      int u = e.src;
+      int v = e.dest;
+      int wt = e.wt;
+
+      if(dist[u]+wt < dist[v]){
+        dist[v] = dist[u] + wt;
+        pq.add(new Pair(v, dist[v]));
+              }
+            }
+          }
+        }
+      for(int i=0;i<dist.length;i++){
+        System.out.print(dist[i]+" ");
+      }  
+      System.out.println();
+  }
+
+
   public static void main(String[] args) {
     int V = 6;
      ArrayList<Edge> graph[] = new ArrayList[V];
     createGraph(graph);
-   
+   int src = 0;
+   dijkstra(graph, src);
   }
 }
